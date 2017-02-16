7544835a571cefdabd6eebb13ce6ac98c6be11cb
keisukeponpoko <k.suke.jp1990@gmail.com>
2017-02-16 23:07:59 +0900

    init

--- darts/settings.py
+++ darts/settings.py
@@ -136,3 +136,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
